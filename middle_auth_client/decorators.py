67c1495f3e074f67a207682850ef5e67054338e5
Chris Jordan <jordanchriss@gmail.com>
2019-06-13 15:25:16 -0400

    added support for direct browser access, using cookies to prevent needing to constantly reauthenticate.

--- middle_auth_client/decorators.py
+++ middle_auth_client/decorators.py
@@ -3,6 +3,7 @@ import flask
 import json
 import os
 import redis
+from urllib.parse import quote
 
 r = redis.Redis(
         host=os.environ.get('REDISHOST', 'localhost'),
@@ -19,27 +20,49 @@ def auth_required(f):
             # this allows auth_required to be an optional decorator if auth_requires_role is also used
             return f(*args, **kwargs)
 
-        token = flask.request.headers.get('authorization')
-        if not token:
-            resp = flask.Response("Unauthorized", 401)
-            resp.headers['WWW-Authenticate'] = 'Bearer realm="' + AUTH_URI + '"'
-            return resp
-        elif not token.startswith('Bearer '):
-            resp = flask.Response("Invalid Request", 400)
-            resp.headers['WWW-Authenticate'] = 'Bearer realm="' + AUTH_URI + '", error="invalid_request", error_description="Header must begin with \'Bearer\'"'
+        token = None
+        update_cookie = False
+        cookie_name = 'middle_auth_token'
+
+        if flask.request.environ.get('HTTP_ORIGIN'): # cors request
+            auth_header = flask.request.headers.get('authorization')
+
+            if not auth_header:
+                resp = flask.Response("Unauthorized", 401)
+                resp.headers['WWW-Authenticate'] = 'Bearer realm="' + AUTH_URI + '"'
+                return resp
+            elif not auth_header.startswith('Bearer '):
+                resp = flask.Response("Invalid Request", 400)
+                resp.headers['WWW-Authenticate'] = 'Bearer realm="' + AUTH_URI + '", error="invalid_request", error_description="Header must begin with \'Bearer\'"'
+                return resp
+
+            token = auth_header.split(' ')[1] # remove schema
+        else: # non cors i.e. direct browser access
+            cookie_token = flask.request.cookies.get(cookie_name)
+            query_param_token = flask.request.args.get('token')
+
+            update_cookie = cookie_token != query_param_token # if token changes, update it
+
+            token = query_param_token or cookie_token
+
+            if not token:
+                return flask.redirect('https://' + AUTH_URI + '/authorize?redirect=' + quote(flask.request.url), code=302)
+
+        cached_user_data = r.get("token_" + token)
+
+        if cached_user_data:
+            flask.g.auth_user = json.loads(cached_user_data.decode('utf-8'))
+            flask.g.auth_token = token
+            resp = f(*args, **kwargs)
+
+            if update_cookie:
+                resp.set_cookie(cookie_name, token, secure=True, httponly=True)
+
             return resp
         else:
-            token = token.split(' ')[1] # remove schema
-            cached_user_data = r.get("token_" + token)
-
-            if cached_user_data:
-                flask.g.auth_user = json.loads(cached_user_data.decode('utf-8'))
-                flask.g.auth_token = token
-                return f(*args, **kwargs)
-            else:
-                resp = flask.Response("Invalid/Expired Token", 401)
-                resp.headers['WWW-Authenticate'] = 'Bearer realm="' + AUTH_URI + '", error="invalid_token", error_description="Invalid/Expired Token"'
-                return resp
+            resp = flask.Response("Invalid/Expired Token", 401)
+            resp.headers['WWW-Authenticate'] = 'Bearer realm="' + AUTH_URI + '", error="invalid_token", error_description="Invalid/Expired Token"'
+            return resp
     return decorated_function
 
 def auth_requires_roles(*required_roles):
