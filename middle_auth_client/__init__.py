f307d9ce97d9e13cc2c129612bcbc90f719e109a
Forrest Collman <forrest.collman@gmail.com>
2021-06-23 21:09:48 -0700

    adding user_has_permission

--- middle_auth_client/__init__.py
+++ middle_auth_client/__init__.py
@@ -1,2 +1,2 @@
 __version__ = '3.4.0'
-from middle_auth_client.decorators import auth_required, auth_requires_admin, auth_requires_permission, get_usernames
+from middle_auth_client.decorators import auth_required, auth_requires_admin, auth_requires_permission, get_usernames, dataset_from_table_id, user_has_permission
